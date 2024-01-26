9203987f185418070d8eb2322b7be31bbca4852f
emersonslfreire <emersonsilvalfreire@hotmail.com>
2024-01-26 16:48:50 -0300

    Final Project

--- src/controllers/pageControllers.ts
+++ src/controllers/pageControllers.ts
@@ -1,14 +1,49 @@
 import { Request , Response } from "express";
-
+import { createMenuObject } from "../helpers/createMenuObject";
+import  {Pet} from '../models/Pet'
 export const home = (req: Request , res: Response)=>{
-    res.send('Home no controller')
+    let list = Pet.getAll()
+    
+    res.render('pages/page',{
+        menu:createMenuObject('all'),
+        banner:{
+            title:'Todos os Animais',
+            background: 'allanimals.jpg'
+        },
+        list 
+    })
 }
 export const dogs = (req: Request , res: Response)=>{
-    res.send('Doguinhos')
+    let list = Pet.getFromType('dog')
+    res.render('pages/page',{
+        menu:createMenuObject('dog'),
+        banner:{
+            title:'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+
+    })
 }
 export const cats = (req: Request , res: Response)=>{
-    res.send('GAtinhos')
+    let list = Pet.getFromType('cat')
+    res.render('pages/page',{
+        menu:createMenuObject('cat'),
+        banner:{
+            title:'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    })
 }
 export const fishes = (req: Request , res: Response)=>{
-    res.send('Peixinhos')
+    let list = Pet.getFromType('fish')
+    res.render('pages/page',{
+        menu:createMenuObject('fish'),
+        banner:{
+            title:'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
-import mustche from 'mustache-express'
+import mustache from 'mustache-express'
 import mainRoutes from './routes/index'
+
 
 dotenv.config();
 
 const server = express()
 
-server.set('view engine ', 'mustache')
-server.set('views', path.join(__dirname, 'views'))
-server.engine('mustache' , mustche())
+server.set('view engine', 'mustache')
+server.set('views' ,path.join(__dirname ,'views'))
+server.engine('mustache' , mustache())
 
 server.use(express.static(path.join(__dirname , '../public')))
 
@@ -19,7 +20,7 @@ server.use(express.static(path.join(__dirname , '../public')))
 server.use(mainRoutes)
 
 server.use((req, res)=>{
-    res.send('Pagina não encontrada')
+    res.render('pages/404')
 })
 
 server.listen(process.env.PORT)
